8aec20b0c962117f816b915295c096be786a0459
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-13 16:46:42 +0600

    Carousel and about us section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Banner from "../Banner/Banner";
+import About from "../About/About";
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home</h1>
+      <Banner />
+      <About />
     </div>
   );
 };
