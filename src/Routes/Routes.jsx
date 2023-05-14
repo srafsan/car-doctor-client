83f9d55bb0af600d03f9f87c3fee2c4006d0b639
Syed Rafsan Raiyan <srafsan98@tutanota.com>
2023-05-14 17:41:18 +0600

    Booking services Added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,7 +3,6 @@ import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/Login/Login";
 import SignUp from "../pages/SignUp/SignUp";
-import CheckOut from "../pages/CheckOut/CheckOut";
 import BookService from "../pages/BookService/BookService";
 
 const router = createBrowserRouter([
@@ -24,8 +23,8 @@ const router = createBrowserRouter([
         element: <SignUp />,
       },
       {
-        path: "/checkout/:id",
-        element: <CheckOut />,
+        path: "/book/:id",
+        element: <BookService />,
         loader: ({ params }) =>
           fetch(`http://localhost:5000/services/${params.id}`),
       },
---
+++ src/pages/BookService/BookService.jsx
@@ -0,0 +1,101 @@
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+
+const CheckOut = () => {
+  const service = useLoaderData();
+  const { _id, title, price, img } = service;
+  const { user } = useContext(AuthContext);
+
+  const handleCheckOut = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = user?.email;
+    const booking = {
+      service_id: _id,
+      service: title,
+      name,
+      email,
+      img,
+      date,
+      price,
+    };
+
+    fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          alert("Service booked successfully");
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center font-bold">Book Service: {title}</h1>
+      <form onSubmit={handleCheckOut}>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              defaultValue={user?.displayName}
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Date</span>
+            </label>
+            <input type="date" name="date" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              defaultValue={user?.email}
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Due Amount</span>
+            </label>
+            <input
+              type="number"
+              placeholder="Amount"
+              name="amount"
+              defaultValue={price}
+              className="input input-bordered"
+            />
+          </div>
+        </div>
+        <div className="form-control mt-6">
+          <input
+            type="submit"
+            value="Order Confirm"
+            className="btn btn-warning btn-block"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CheckOut;
